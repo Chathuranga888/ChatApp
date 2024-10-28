c669190719e3eca5450faff905d2477b6fce27bd
Your Name <you@example.com>
2024-10-28 21:36:36 +0530

    setup basic nodejs app using express

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
 
-require('dotenv').config();
-
+dotenv.config();
 const apiKey = process.env.OPENAI_API_KEY;
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req,res) => {
+  res.render("index.ejs", {
+    content: "Hello World"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listen at port ${port}`);
+});
